076e18103049f28f6b05983dc83e6a454d55a8d9
aurorpar <aurorapariseau@gmail.com>
2025-05-08 13:42:58 -0500

    Implementation ready for testing.

---
+++ src/main.py
@@ -0,0 +1,122 @@
+import os
+from time import sleep
+import traceback
+from multiprocessing import Process
+from threading import Thread
+
+INSTALL_LOCATION = r"C:\arksa\ShooterGame\Binaries\Win64\ArkAscendedServer.exe"
+CLUSTER_ID = "SpawningPool"
+MAPS_AND_PORTS = {
+    "The Center": {
+        "map": "TheCenter_WP",
+        "ports": [7777, 7778, 27015]
+    },
+    "The Island": {
+        "map": "TheIsland_WP",
+        "ports": [7779, 7780, 27016]
+    },
+}
+SETTINGS = [
+    'NoTransferFromFiltering',
+    'ForceAllowCaveFlyers',
+    'NoBattlEye'
+]
+MODS = [
+    928603,  # Easy Mutations
+    929420   # Super SpyGlass Plus
+]
+
+
+# THIS IS NOT A SETTING. DO NOT CHANGE
+EXIT = False
+
+
+def main():
+
+    server_instances = {}
+    checking_thread = Thread(target=check_running_servers, args=(server_instances,))
+    checking_thread.start()
+
+    while True:
+
+        print(f"Last known running servers:")
+        for running_map_name in server_instances.keys():
+            print(f"\n\t{running_map_name}")
+
+        if not os.path.exists(INSTALL_LOCATION):
+            input("Could not find server executable. Check installation directory. Press enter to close.")
+            close()
+
+        map_name, selected_map = prompt_for_map()
+        if map_name in server_instances.keys():
+            print("That map is already running. Restart all servers and the manager if you're having problems.")
+            close()
+
+        command = f"start {INSTALL_LOCATION} " + \
+                  f"{selected_map['map']}"
+        command += f'?GameServerQueryPort={selected_map['ports'][2]}'
+
+        for x in SETTINGS:
+            command += f' -{x}'
+        command += f' -port={selected_map['ports'][0]}'
+
+        if MODS:
+            command += " -mods=\"" + ",".join(list(map(str, MODS))) + "\""
+
+        print(f"Starting map {map_name}. Startup settings:\n\t{command}")
+        server_instance = Process(target=os.system, args=(command,))
+        server_instance.start()
+        server_instances[map_name] = server_instance
+
+
+def prompt_for_map():
+    server_selection = None
+    ark_maps = list(MAPS_AND_PORTS.keys())
+
+    while not server_selection:
+        print("Please select the desired map: ")
+        for map_number, ark_map in enumerate(ark_maps):
+            print(f"\n\t{map_number}. {ark_map}")
+        server_selection = input()
+
+        if not server_selection.isdigit():
+            print("You did not select a valid choice")
+            continue
+        server_selection = int(server_selection)
+        if not 0 < server_selection <= len(ark_maps):
+            print("You did not select a valid choice")
+            continue
+
+        break
+
+    server_selection = ark_maps[server_selection-1]
+    return server_selection, MAPS_AND_PORTS[server_selection]
+
+
+def check_running_servers(server_instances):
+    while True:
+        if EXIT:
+            return
+        sleep(1)
+        prunes = []
+        for map_name, process in server_instances.items():
+            process.join(timeout=0)
+            if not process.is_alive():
+                print(f"Map {map_name} is no longer running")
+                prunes.append(map_name)
+        for prune in prunes:
+            del server_instances[prune]
+
+
+def close():
+    global EXIT
+    EXIT = True
+    exit(1)
+
+
+if __name__ == "__main__":
+    try:
+        main()
+    except Exception as e:
+        print("".join(traceback.format_exception(e)))
+        input()
